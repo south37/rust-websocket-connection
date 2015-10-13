4e1b8253bc5aab9ea5cf167c0c558b46a55591d6
Nao Minami <south37777@gmail.com>
2015-10-13 09:09:13 +0900

    Add event_loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+extern crate mio;
+use mio::*;
+use mio::tcp::*;
+
+struct WebSocketServer;
+impl Handler for WebSocketServer {
+  type Timeout = usize;
+  type Message = ();
+}
+
 fn main() {
-    println!("Hello, world!");
+  let mut event_loop = EventLoop::new().unwrap();
+
+  let address = "0.0.0.0:10000".parse().unwrap();
+  let server_socket = TcpListener::bind(&address).unwrap();
+
+  event_loop.register(&server_socket,
+             Token(0),
+             EventSet::readable(),
+             PollOpt::edge()).unwrap();
+
+  let mut handler = WebSocketServer;
+  event_loop.run(&mut handler).unwrap();
 }
