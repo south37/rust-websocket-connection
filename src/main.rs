191ae9225980f9be79597ce341fe7b46cf7278ee
Nao Minami <south37777@gmail.com>
2015-10-18 14:34:11 +0900

    Delete use

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
 extern crate mio;
-use std::net::SocketAddr;
-use mio::*;
-use mio::tcp::*;
 
 struct WebSocketServer;
-impl Handler for WebSocketServer {
+impl mio::Handler for WebSocketServer {
   type Timeout = usize;
   type Message = ();
 }
 
 fn main() {
-  let mut event_loop = EventLoop::new().unwrap();
+  let mut event_loop = mio::EventLoop::new().unwrap();
 
-  let address = "0.0.0.0:10000".parse::<SocketAddr>().unwrap();
-  let server_socket = TcpListener::bind(&address).unwrap();
+  let address = "0.0.0.0:10000".parse::<std::net::SocketAddr>().unwrap();
+  let server_socket = mio::tcp::TcpListener::bind(&address).unwrap();
 
   event_loop.register(&server_socket,
-             Token(0),
-             EventSet::readable(),
-             PollOpt::edge()).unwrap();
+             mio::Token(0),
+             mio::EventSet::readable(),
+             mio::PollOpt::edge()).unwrap();
 
   let mut handler = WebSocketServer;
   event_loop.run(&mut handler).unwrap();
