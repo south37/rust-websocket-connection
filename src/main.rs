d2c2877336f5f87b2deb584f1c2000f70b83fb3c
Nao Minami <south37777@gmail.com>
2015-10-18 19:19:41 +0900

    Add sha1 and rustc_serialize

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
 extern crate mio;
-extern crate http_muncher;
 use mio::TryRead;
+extern crate http_muncher;
+extern crate sha1;
+extern crate rustc_serialize;
+use rustc_serialize::base64::ToBase64;
+
+fn gen_key(key: &String) -> String {
+    let mut m = sha1::Sha1::new();
+    let mut buf = [0u8; 20];
+
+    m.update(key.as_bytes());
+    m.update("258EAFA5-E914-47DA-95CA-C5AB0DC85B11".as_bytes());
+
+    m.output(&mut buf);
+
+    return buf.to_base64(rustc_serialize::base64::STANDARD);
+}
 
 struct HttpParser;
 impl http_muncher::ParserHandler for HttpParser { }
